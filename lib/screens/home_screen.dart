b40bcf99fe5af1a8c7e88b2deba6c027adc9f772
Hansel <hgl_22@hotmail.es>
2022-03-12 19:30:32 -0600

    ajustando iconon en la card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,66 +6,103 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppbarMethod(),
-          body: ListView(
-            children: [
-              Container(
-                height: 337,
-                width: 346,
-                margin: EdgeInsets.symmetric(vertical: 3.5, horizontal: 15),
-                child: Card(
-                    elevation: 12,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                    clipBehavior: Clip.antiAlias,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(15),
-                              bottomRight: Radius.circular(15)),
-                          child: Container(
-                            margin: EdgeInsets.symmetric(vertical: 2.0),
-                            child: FadeInImage(
-                              placeholder: NetworkImage(
-                                  'https://via.placeholder.com/315x189'),
-                              image: NetworkImage(
-                                  'https://via.placeholder.com/315x189'),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Color.fromARGB(232, 253, 253, 253),
-                              boxShadow: [
-                                new BoxShadow(
-                                  color: Colors.black54,
-                                  offset: new Offset(0.0, 4.0),
-                                  blurRadius: 4.0,
-                                ),
-                              ],
-                            ),
-                            // decoration: BoxDecoration(color: Colors.black),
-                          ),
+        resizeToAvoidBottomInset: false,
+        appBar: AppbarMethod(),
+        body: MethodListView(),
+      ),
+    );
+  }
+
+  ListView MethodListView() {
+    return ListView(
+      children: [
+        Container(
+          height: 337,
+          width: 346,
+          margin: EdgeInsets.symmetric(vertical: 3.5, horizontal: 25),
+          child: Card(
+            elevation: 12,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            clipBehavior: Clip.antiAlias,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ContainerCustomImageCard(),
+                  Container(
+                    child: ListTile(
+                      title: FittedBox(
+                          child: Text("Título del artículo publicado",
+                              style: GoogleFonts.roboto())),
+                      subtitle: FittedBox(
+                        alignment: Alignment.centerLeft,
+                        fit: BoxFit.contain,
+                        child: Text(
+                          "Previsualización de los detalles del artículo con información en formato completo",
+                          style: GoogleFonts.roboto(),
                         ),
-                        Container(
-                          child: ListTile(
-                            leading: Icon(Icons.arrow_back_ios_new_outlined),
-                            title: Text("Texto 1"),
-                            subtitle: Text("Texto 2"),
-                          ),
-                        ),
-                        ButtonBar(
-                          alignment: MainAxisAlignment.start,
-                          children: [
-                            TextButton(
-                              child: Text("Hola"),
-                              onPressed: () {},
-                            ),
-                          ],
-                        )
-                      ],
-                    )),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text("C99,500.00",
+                        style: GoogleFonts.roboto(
+                            color: Colors.red[700],
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500)),
+
+                    //color: Colors.amberAccent,
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  ),
+                  Image.asset('assets/carrito.ico')
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container ContainerCustomImageCard() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+        color: Color.fromARGB(106, 253, 253, 253),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.black26,
+            spreadRadius: -1,
+            offset: new Offset(1, 3.0),
+            blurRadius: 0.5,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(106, 253, 253, 253),
+            boxShadow: [
+              new BoxShadow(
+                color: Colors.black26,
+                offset: new Offset(1, 3.0),
+                blurRadius: 0.5,
               ),
             ],
-          )),
+          ),
+          margin: EdgeInsets.symmetric(vertical: 2.0),
+          child: FadeInImage(
+            placeholder: NetworkImage('https://via.placeholder.com/315x189'),
+            image: NetworkImage('https://via.placeholder.com/315x189'),
+          ),
+
+          // decoration: BoxDecoration(color: Colors.black),
+        ),
+      ),
     );
   }
 
