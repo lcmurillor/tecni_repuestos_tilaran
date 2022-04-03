ec5e35421d08d8964c477e9a5c81fa01ba21bde4
Hansel <hgl_22@hotmail.es>
2022-04-03 15:49:13 -0600

    distribuyendo widgets en sus respectivas carpetas

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,36 +16,3 @@ class LoginScreen extends StatelessWidget {
     ));
   }
 }
-
-class CardLogin extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 160, horizontal: 30),
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.topCenter,
-              width: 342,
-              height: 400,
-              child: Container(
-                margin: EdgeInsets.only(top: 15),
-                child: Text('Iniciar Sesión',
-                    style: GoogleFonts.roboto(
-                      fontSize: 44,
-                      fontWeight: FontWeight.w600,
-                    )),
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: Colors.white,
-                boxShadow: [MainTheme.cardShadow],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
