07155189dbee58425264f6ab6d00ad591679e1aa
Hansel <hgl_22@hotmail.es>
2022-05-08 17:22:07 -0600

    ajustes en iniciales en circleavatar e info de profilescreen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,6 +5,7 @@ import 'package:tecni_repuestos/Services/services.dart';
 import 'package:tecni_repuestos/models/models.dart';
 import 'package:tecni_repuestos/theme/themes.dart';
 import 'package:tecni_repuestos/widgets/widgets.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -55,8 +56,9 @@ class ProfileScreen extends StatelessWidget {
                   alignment: Alignment.center,
                   child: CircleAvatar(
                     child: Text(
-                      //TODO Seleccionar solo la prinmera lera de nombre y apellido
-                      user[0].name,
+                      user[0].name.substring(0, 1).toUpperCase() +
+                          '' +
+                          user[0].lastname.substring(0, 1).toUpperCase(),
                       style: CustomTextStyle.robotoMedium
                           .copyWith(fontSize: 50, color: Colors.white),
                     ),
@@ -65,19 +67,22 @@ class ProfileScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20),
-                Text(user[0].name + ' ' + user[0].lastname,
+                Text(
+                    user[0].name.toUpperCase() +
+                        ' ' +
+                        user[0].lastname.toUpperCase(),
                     style: CustomTextStyle.robotoExtraBold
                         .copyWith(fontSize: 20, color: Colors.black)),
                 const SizedBox(
                   height: 7,
                 ),
-                Text('dirección@correo.com',
+                Text(user[0].email,
                     style: CustomTextStyle.robotoSemiBold
                         .copyWith(fontSize: 15, color: Colors.grey[500])),
                 const SizedBox(
                   height: 7,
                 ),
-                Text('+50688888888',
+                Text('+506 ' + user[0].phone,
                     style: CustomTextStyle.robotoSemiBold
                         .copyWith(fontSize: 16, color: ColorStyle.mainRed)),
                 const SizedBox(
@@ -87,11 +92,11 @@ class ProfileScreen extends StatelessWidget {
                     style: CustomTextStyle.robotoExtraBold
                         .copyWith(fontSize: 15, color: Colors.grey[500])),
                 const SizedBox(height: 20),
-                //TODO agregar iconos mas adecuados no impor la libreria
+                //TODO agregar iconos mas adecuados no importa la libreria
                 _CustomInfoButton(
                   icon: MdiIcons.accountEdit,
                   onPressed: () {
-                    //TODO crearo los metodos de navegacion
+                    //TODO crear los metodos de navegacion
                     print("Esto es un boton 1 ");
                   },
                   text: 'Edtar mi información',
@@ -144,7 +149,7 @@ class _CustomInfoButton extends StatelessWidget {
               child: Row(
                 children: [
                   Padding(
-                      padding: const EdgeInsets.only(left: 0, right: 10),
+                      padding: const EdgeInsets.only(left: 15, right: 10),
                       child: Container(
                         height: 30,
                         width: 30,
